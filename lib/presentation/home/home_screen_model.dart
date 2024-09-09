7d2763e2d9e8ab329fb5a430916f9a481c67de74
kimhyungmin <jeen14rla@gmail.com>
2024-09-09 22:59:58 +0900

    feat: 인스턴스 변경

--- lib/presentation/home/home_screen_model.dart
+++ lib/presentation/home/home_screen_model.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_flash_card/presentation/card_list/card_list_screen.dart';
-import 'package:flutter_flash_card/presentation/components/recent_used_card_list/recent_used_card_list.dart';
 import 'package:flutter_flash_card/presentation/my_info/my_info_screen.dart';
+import 'package:flutter_flash_card/presentation/recent_used_card_list/recent_used_card_list_screen.dart';
 
 class HomeScreenModel extends ChangeNotifier {
   int _currentPageIndex = 0;
 
   final List<Widget> _screens = const [
-    RecentUsedCardList(),
+    RecentUsedCardListScreen(),
     CardListScreen(),
     MyInfoScreen(),
   ];
