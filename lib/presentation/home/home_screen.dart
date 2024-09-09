c82eeee7b8fe6541d80859e36e2d18eddb3e1250
kimhyungmin <jeen14rla@gmail.com>
2024-09-09 15:56:29 +0900

    feat: add temp home ui

--- lib/presentation/home/home_screen.dart
+++ lib/presentation/home/home_screen.dart
@@ -1,10 +1,39 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flash card'),
+        centerTitle: true,
+      ),
+      body: ListView(
+        children: [
+          Text('최근 사용 내역이 없습니다')
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: '홈',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.folder_rounded),
+          label: '카드',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person),
+          label: '내 정보',
+        ),
+      ]),
+    );
   }
 }
