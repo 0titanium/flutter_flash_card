649de7ada5cf821b0365eacac04647b3286c42df
kimhyungmin <jeen14rla@gmail.com>
2024-09-09 20:58:23 +0900

    feat: 홈 스크린 모델 적용

--- lib/presentation/home/home_screen.dart
+++ lib/presentation/home/home_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_flash_card/presentation/components/flash_card_bottom/flash_card_bottom.dart';
+import 'package:flutter_flash_card/presentation/home/home_screen_model.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,45 +11,19 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int currentPageIndex = 0;
-
   @override
   Widget build(BuildContext context) {
+    final homeScreenModel = context.watch<HomeScreenModel>();
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flash card'),
         centerTitle: true,
       ),
-      body: const Padding(
-        padding: EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            Card(
-              child: ListTile(
-                title: Text('sample1'),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                title: Text('sample2'),
-              ),
-            ),
-          ],
-        ),
-      ),
-      bottomNavigationBar: NavigationBar(
-        onDestinationSelected: (int index) {
-          setState(() {
-            currentPageIndex = index;
-          });
-        },
-        indicatorColor: Colors.green,
-        selectedIndex: currentPageIndex,
-        destinations: const [
-          NavigationDestination(icon: Icon(Icons.home), label: '홈'),
-          NavigationDestination(icon: Icon(Icons.folder_rounded), label: '카드'),
-          NavigationDestination(icon: Icon(Icons.person), label: '내 정보'),
-        ],
+      body: homeScreenModel.screens[homeScreenModel.currentPageIndex],
+      bottomNavigationBar: FlashCardBottom(
+        currentPageIndex: homeScreenModel.currentPageIndex,
+        onIndexSelected: homeScreenModel.onIndexSelected,
       ),
     );
   }
