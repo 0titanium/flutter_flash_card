18616e44852a2777b170cbe1542272524da0425f
kimhyungmin <jeen14rla@gmail.com>
2024-09-10 20:03:51 +0900

    feat: 임시 덱 추가 스크린 전환 추가

--- lib/presentation/card_list/card_list_screen.dart
+++ lib/presentation/card_list/card_list_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class CardListScreen extends StatefulWidget {
   const CardListScreen({super.key});
@@ -14,6 +15,20 @@ class _CardListScreenState extends State<CardListScreen> {
       appBar: AppBar(
         title: const Text('카드 목록'),
         centerTitle: true,
+      ),
+      body: ListView(
+        children: [
+          Card(
+            child: ListTile(
+              titleAlignment: ListTileTitleAlignment.center,
+              title: const Text('덱 추가'),
+              leading: const Icon(Icons.add),
+              onTap: () {
+                context.go('/card_list/adding_card_deck');
+              },
+            ),
+          )
+        ],
       ),
     );
   }
