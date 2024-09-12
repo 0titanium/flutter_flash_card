d07aeb68364a9cc53ed6e673fec8ae84d8d182c8
kimhyungmin <jeen14rla@gmail.com>
2024-09-12 18:14:23 +0900

    chore: 파일 명 변경

--- lib/presentation/home/home_screen_model.dart
+++ lib/presentation/home/home_screen_model.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_flash_card/presentation/folder_list/card_list_screen.dart';
+import 'package:flutter_flash_card/presentation/folder_list/folder_list_screen.dart';
 import 'package:flutter_flash_card/presentation/my_info/my_info_screen.dart';
 import 'package:flutter_flash_card/presentation/recent_used_card_list/recent_used_card_list_screen.dart';
 
