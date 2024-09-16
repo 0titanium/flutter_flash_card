ef27e343a6e1b9667ac047bb12e51931ffff97f8
kimhyungmin <jeen14rla@gmail.com>
2024-09-16 23:04:08 +0900

    chore: 미사용 import 제거

--- lib/presentation/add_folder/add_folder_dialog.dart
+++ lib/presentation/add_folder/add_folder_dialog.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_flash_card/presentation/add_folder/add_folder_model.dart';
 import 'package:flutter_flash_card/presentation/folder_list/folder_list_model.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
