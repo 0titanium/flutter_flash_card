926d08b203c6854a1602b3b121418ad743e0c11a
kimhyungmin <jeen14rla@gmail.com>
2024-09-16 17:41:30 +0900

    fix: ListView.builder로 변경

--- lib/presentation/folder_list/folder_list_screen.dart
+++ lib/presentation/folder_list/folder_list_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_flash_card/presentation/add_folder/add_folder_dialog.dart';
 import 'package:flutter_flash_card/presentation/folder_list/folder_list_model.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
@@ -14,31 +15,43 @@ class _FolderListScreenState extends State<FolderListScreen> {
   @override
   Widget build(BuildContext context) {
     final folderListModel = context.watch<FolderListModel>();
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('폴더 목록'),
-        centerTitle: true,
-      ),
-      body: ListView(
-        children: [
-          Card(
-            child: ListTile(
-              titleAlignment: ListTileTitleAlignment.center,
-              title: const Text('폴더 추가'),
-              leading: const Icon(Icons.add),
-              onTap: () {
-                context.go('/folder_list/add_folder');
-              },
-            ),
-          ),
-          ...folderListModel.folders.map(
-            (folder) => Card(
-              child: ListTile(
-                title: Text(folder.name),
-              ),
-            ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              // showDialog(
+              //   context: context,
+              //   builder: (context) => AddFolderDialog(),
+              // );
+              context.go('/folder_list/add_folder');
+            },
+            icon: const Icon(Icons.create_new_folder),
           ),
         ],
+        centerTitle: true,
+      ),
+      body: ListView.builder(
+        padding: const EdgeInsets.all(8),
+        itemCount: folderListModel.folders.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: ListTile(
+              title: Text(folderListModel.folders[index].name),
+            ),
+          );
+        },
+        // children: [
+        //   ...folderListModel.folders.map(
+        //     (folder) => Card(
+        //       child: ListTile(
+        //         title: Text(folder.name),
+        //       ),
+        //     ),
+        //   ),
+        // ],
       ),
     );
   }
