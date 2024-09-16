90d9ec9b29cc34cd12854ebbdc1cbf89c7ac5e6b
kimhyungmin <jeen14rla@gmail.com>
2024-09-16 23:05:01 +0900

    feat: 라우터 변경

--- lib/core/router/router.dart
+++ lib/core/router/router.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_flash_card/data/local/data_service.dart';
 import 'package:flutter_flash_card/presentation/add_folder/add_folder_dialog.dart';
-import 'package:flutter_flash_card/presentation/card_deck/card_deck_screen.dart';
 import 'package:flutter_flash_card/presentation/components/flash_card_bottom/flash_card_bottom.dart';
 import 'package:flutter_flash_card/presentation/components/flash_card_bottom/flash_card_bottom_model.dart';
+import 'package:flutter_flash_card/presentation/folder/folder_model.dart';
+import 'package:flutter_flash_card/presentation/folder/folder_screen.dart';
 import 'package:flutter_flash_card/presentation/folder_list/folder_list_model.dart';
 import 'package:flutter_flash_card/presentation/folder_list/folder_list_screen.dart';
 import 'package:flutter_flash_card/presentation/home/home_screen.dart';
@@ -59,9 +60,12 @@ final GoRouter router = GoRouter(
               },
             ),
             GoRoute(
-              path: 'card_deck',
+              path: 'folder/:folderName',
               builder: (context, state) {
-                return const CardDeckScreen();
+                return ChangeNotifierProvider(
+                  create: (_) => FolderModel(folderName: state.extra as String),
+                  child: const FolderScreen(),
+                );
               },
             ),
           ],
