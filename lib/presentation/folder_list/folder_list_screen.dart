d81ca2df07dc64275a6b10702a9375debf357160
kimhyungmin <jeen14rla@gmail.com>
2024-09-20 17:08:18 +0900

    feat: 폴더 아이디 전달 추가

--- lib/presentation/folder_list/folder_list_screen.dart
+++ lib/presentation/folder_list/folder_list_screen.dart
@@ -42,21 +42,15 @@ class _FolderListScreenState extends State<FolderListScreen> {
               onTap: () {
                 context.go(
                   '/folder_list/folder/:${folderListModel.folders[index].name}',
-                  extra: folderListModel.folders[index].name,
+                  extra: [
+                    folderListModel.folders[index].name,
+                    folderListModel.folders[index].id,
+                  ],
                 );
               },
             ),
           );
         },
-        // children: [
-        //   ...folderListModel.folders.map(
-        //     (folder) => Card(
-        //       child: ListTile(
-        //         title: Text(folder.name),
-        //       ),
-        //     ),
-        //   ),
-        // ],
       ),
     );
   }
