a0536a27b20ee39893610ea6d0d36e805c0afc9a
kimhyungmin <jeen14rla@gmail.com>
2024-09-20 17:06:29 +0900

    feat: 폴더 모델 덱 로드 추가

--- lib/presentation/folder/folder_model.dart
+++ lib/presentation/folder/folder_model.dart
@@ -1,11 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_flash_card/data/local/data_service.dart';
+import 'package:flutter_flash_card/domain/model/deck.dart';
 
 class FolderModel extends ChangeNotifier {
-  final String folderName;
+  final List<String> folderData;
+  final DataService _dataService;
+
+  List<Deck> _decks = [];
+
+  List<Deck> get decks => _decks;
 
   FolderModel({
-    required this.folderName,
-  });
+    required this.folderData,
+    required DataService dataService,
+  }) : _dataService = dataService {
+    loadDecks();
+  }
 
+  Future<void> loadDecks() async {
+    final folderId = folderData[1];
+    final rootFolder = await _dataService.loadRootFolder();
+    final nowFolder = _dataService.findFolder(rootFolder, folderId);
 
+    if (nowFolder != null) {
+      _decks = nowFolder.decks;
+    }
+  }
 }
