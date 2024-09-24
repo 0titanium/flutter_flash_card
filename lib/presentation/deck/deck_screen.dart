677022a52f095fd765926797db05bd61845a03ff
kimhyungmin <jeen14rla@gmail.com>
2024-09-24 14:08:21 +0900

    feat: 덱 스크린 추가

---
+++ lib/presentation/deck/deck_screen.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_flash_card/core/router/router.dart';
+import 'package:flutter_flash_card/presentation/deck/deck_model.dart';
+import 'package:provider/provider.dart';
+
+class DeckScreen extends StatefulWidget {
+  const DeckScreen({super.key});
+
+  @override
+  State<DeckScreen> createState() => _DeckScreenState();
+}
+
+class _DeckScreenState extends State<DeckScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final deckModel = context.watch<DeckModel>();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Card list'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              // context.go(
+              //   '/folder_list/folder/${folderModel.folderData[0]}/add_deck',
+              //   extra: folderModel.folderData,
+              // );
+            },
+            icon: const Icon(Icons.archive),
+          ),
+        ],
+        centerTitle: true,
+      ),
+      body: ListView.builder(
+        padding: const EdgeInsets.all(8),
+        itemCount: deckModel.cards.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: ListTile(
+              // title: Text(deckModel.cards[index].front),
+              onTap: () {
+                // context.go(
+                //   '/folder_list/folder/:${folderModel.decks[index].deckName}',
+                //   extra: [
+                //     folderModel.decks[index].deckName,
+                //     folderModel.decks[index].id,
+                //   ],
+                // );
+              },
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
