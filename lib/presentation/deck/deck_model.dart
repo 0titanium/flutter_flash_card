e253a8d0f60292cb032bb2f74b80faa2f35fd459
kimhyungmin <jeen14rla@gmail.com>
2024-09-25 17:12:14 +0900

    feat: 덱 모델 수정

--- lib/presentation/deck/deck_model.dart
+++ lib/presentation/deck/deck_model.dart
@@ -3,14 +3,27 @@ import 'package:flutter_flash_card/data/local/data_service.dart';
 import 'package:flutter_flash_card/domain/model/deck.dart';
 
 class DeckModel extends ChangeNotifier {
+  final List<dynamic> data;
   final DataService _dataService;
-  final List<String>_deckData;
+  List<Deck> _deckData = [];
+
+  final TextEditingController cardFrontController = TextEditingController();
+  final TextEditingController cardBackController = TextEditingController();
 
   List<Card> _cards = [];
 
   List<Card> get cards => _cards;
 
-  DeckModel({required List<String> deckData, required DataService dataService})
-      : _deckData = deckData,
-        _dataService = dataService;
+  DeckModel({
+    required this.data,
+    required DataService dataService,
+  }) : _dataService = dataService {
+    loadCards();
+  }
+
+  Future<void> loadCards() async {
+  }
+
+  Future<void> createCard() async {}
+
 }
