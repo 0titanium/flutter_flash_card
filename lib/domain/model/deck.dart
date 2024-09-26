2840c3224eb0afe07ba063f3a59f79bc240acff3
kimhyungmin <jeen14rla@gmail.com>
2024-09-27 00:16:06 +0900

    feat: 카드 모델 명 변경으로 리빌드

--- lib/domain/model/deck.dart
+++ lib/domain/model/deck.dart
@@ -1,4 +1,4 @@
-import 'package:flutter_flash_card/domain/model/card.dart';
+import 'package:flutter_flash_card/domain/model/learning_card.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'deck.freezed.dart';
@@ -10,7 +10,7 @@ class Deck with _$Deck {
   const factory Deck({
     required String id,
     required String deckName,
-    required List<Card> cards,
+    required List<LearningCard> cards,
   }) = _Deck;
 
   factory Deck.fromJson(Map<String, Object?> json) => _$DeckFromJson(json);
