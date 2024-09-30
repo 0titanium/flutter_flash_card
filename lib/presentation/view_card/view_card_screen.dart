a02b6527a93381bd56421c762a3b10757ed9440b
kimhyungmin <jeen14rla@gmail.com>
2024-09-30 17:37:11 +0900

    feat: 앱바 추가, 높이 변경

--- lib/presentation/view_card/view_card_screen.dart
+++ lib/presentation/view_card/view_card_screen.dart
@@ -15,49 +15,54 @@ class _ViewCardScreenState extends State<ViewCardScreen> {
   Widget build(BuildContext context) {
     final viewCardModel = context.watch<ViewCardModel>();
 
-    return Center(
-      child: SizedBox(
-        height: MediaQuery.of(context).size.height * 0.5,
-        child: PageView.builder(
-          controller: viewCardModel.pageController,
-          itemCount: viewCardModel.cards.length,
-          physics: const ClampingScrollPhysics(),
-          padEnds: true,
-          itemBuilder: (context, index) {
-            return Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Center(
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(10),
-                  child: SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.8,
-                    child: FlipCard(
-                      frontWidget: ColoredBox(
-                        color: Colors.primaries[index % Colors.primaries.length]
-                            .withOpacity(0.5),
-                        child: Center(
-                          child: Padding(
-                            padding: const EdgeInsets.all(16.0),
-                            child: Text(
-                              viewCardModel.cards[index].frontText,
-                              style: const TextStyle(
-                                  color: Colors.white, fontSize: 20),
-                              textAlign: TextAlign.center,
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height * 0.6,
+          child: PageView.builder(
+            controller: viewCardModel.pageController,
+            itemCount: viewCardModel.cards.length,
+            physics: const ClampingScrollPhysics(),
+            padEnds: true,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Center(
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.8,
+                      child: FlipCard(
+                        frontWidget: ColoredBox(
+                          color: Colors
+                              .primaries[index % Colors.primaries.length]
+                              .withOpacity(0.5),
+                          child: Center(
+                            child: Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: Text(
+                                viewCardModel.cards[index].frontText,
+                                style: const TextStyle(
+                                    color: Colors.white, fontSize: 20),
+                                textAlign: TextAlign.center,
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                      backWidget: ColoredBox(
-                        color: Colors.primaries[index % Colors.primaries.length]
-                            .withOpacity(0.3),
-                        child: Center(
-                          child: Padding(
-                            padding: const EdgeInsets.all(16.0),
-                            child: Text(
-                              viewCardModel.cards[index].backText,
-                              style: const TextStyle(
-                                  color: Colors.black, fontSize: 20),
-                              textAlign: TextAlign.center,
+                        backWidget: ColoredBox(
+                          color: Colors
+                              .primaries[index % Colors.primaries.length]
+                              .withOpacity(0.3),
+                          child: Center(
+                            child: Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: Text(
+                                viewCardModel.cards[index].backText,
+                                style: const TextStyle(
+                                    color: Colors.black, fontSize: 20),
+                                textAlign: TextAlign.center,
+                              ),
                             ),
                           ),
                         ),
@@ -65,9 +70,9 @@ class _ViewCardScreenState extends State<ViewCardScreen> {
                     ),
                   ),
                 ),
-              ),
-            );
-          },
+              );
+            },
+          ),
         ),
       ),
     );
