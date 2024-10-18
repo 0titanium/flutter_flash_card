0425ea3a87be848539192915da9fc9e06baae2ae
kimhyungmin <jeen14rla@gmail.com>
2024-10-18 17:33:21 +0900

    feat: 저장된 방문 폴더 로드 메서드, 변수 추가

--- lib/presentation/home/home_screen_model.dart
+++ lib/presentation/home/home_screen_model.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_flash_card/data/local/data_service.dart';
 import 'package:flutter_flash_card/presentation/folder_list/folder_list_screen.dart';
 import 'package:flutter_flash_card/presentation/my_info/my_info_screen.dart';
 import 'package:flutter_flash_card/presentation/recent_used_card_list/recent_used_card_list_screen.dart';
 
 class HomeScreenModel extends ChangeNotifier {
+  final DataService _dataService;
+
   int _currentPageIndex = 0;
 
   final List<Widget> _screens = const [
@@ -16,9 +19,23 @@ class HomeScreenModel extends ChangeNotifier {
 
   List<Widget> get screens => _screens;
 
+  List<String> _folderNames = [];
+
+  List<String> get folderNames => _folderNames;
+
+  HomeScreenModel({required DataService dataService})
+      : _dataService = dataService {
+    loadSavedFolders();
+  }
+
   void onIndexSelected(int index) {
     _currentPageIndex = index;
 
+    notifyListeners();
+  }
+
+  Future<void> loadSavedFolders() async {
+    _folderNames = await _dataService.getVisitedFolders();
     notifyListeners();
   }
 }
