e0838caa3431d57a521145228a964843a0453812
kimhyungmin <jeen14rla@gmail.com>
2024-10-18 17:34:31 +0900

    chore: 불필요한 임포트 제거

--- lib/presentation/home/home_screen.dart
+++ lib/presentation/home/home_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_flash_card/core/router/router.dart';
 import 'package:flutter_flash_card/presentation/home/home_screen_model.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
