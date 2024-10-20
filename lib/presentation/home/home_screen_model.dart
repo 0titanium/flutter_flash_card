33414f9296a6138a494b98c030527957caa305c7
kimhyungmin <jeen14rla@gmail.com>
2024-10-20 17:00:58 +0900

    feat: 방문 폴더 겟 메서드 적용

--- lib/presentation/home/home_screen_model.dart
+++ lib/presentation/home/home_screen_model.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_flash_card/data/local/data_service.dart';
+import 'package:flutter_flash_card/domain/model/folder.dart';
 import 'package:flutter_flash_card/presentation/folder_list/folder_list_screen.dart';
 import 'package:flutter_flash_card/presentation/my_info/my_info_screen.dart';
 import 'package:flutter_flash_card/presentation/recent_used_card_list/recent_used_card_list_screen.dart';
@@ -19,9 +20,9 @@ class HomeScreenModel extends ChangeNotifier {
 
   List<Widget> get screens => _screens;
 
-  List<String> _folderNames = [];
+  List<Folder> _savedFolders = [];
 
-  List<String> get folderNames => _folderNames;
+  List<Folder> get savedFolders => _savedFolders;
 
   HomeScreenModel({required DataService dataService})
       : _dataService = dataService {
@@ -35,7 +36,7 @@ class HomeScreenModel extends ChangeNotifier {
   }
 
   Future<void> loadSavedFolderNames() async {
-    _folderNames = await _dataService.getVisitedFolders();
+    _savedFolders = await _dataService.getVisitedFolders() ?? [];
     notifyListeners();
   }
 }
