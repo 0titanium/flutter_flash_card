4d69d3cc45bebfe2cee1aaff67b7ff5d2cc6a37e
kimhyungmin <jeen14rla@gmail.com>
2024-10-20 17:18:58 +0900

    chore: 불필요한 코드 제거

--- lib/presentation/home/home_screen_model.dart
+++ lib/presentation/home/home_screen_model.dart
@@ -1,24 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_flash_card/data/local/data_service.dart';
 import 'package:flutter_flash_card/domain/model/folder.dart';
-import 'package:flutter_flash_card/presentation/folder_list/folder_list_screen.dart';
-import 'package:flutter_flash_card/presentation/my_info/my_info_screen.dart';
-import 'package:flutter_flash_card/presentation/recent_used_card_list/recent_used_card_list_screen.dart';
 
 class HomeScreenModel extends ChangeNotifier {
   final DataService _dataService;
-
-  int _currentPageIndex = 0;
-
-  final List<Widget> _screens = const [
-    RecentUsedCardListScreen(),
-    FolderListScreen(),
-    MyInfoScreen(),
-  ];
-
-  int get currentPageIndex => _currentPageIndex;
-
-  List<Widget> get screens => _screens;
 
   List<Folder> _savedFolders = [];
 
@@ -27,12 +12,6 @@ class HomeScreenModel extends ChangeNotifier {
   HomeScreenModel({required DataService dataService})
       : _dataService = dataService {
     loadSavedFolderNames();
-  }
-
-  void onIndexSelected(int index) {
-    _currentPageIndex = index;
-
-    notifyListeners();
   }
 
   Future<void> loadSavedFolderNames() async {
