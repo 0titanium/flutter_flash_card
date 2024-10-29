c8104a41da205d12839fbb689cd204908ab3f9f0
kimhyungmin <jeen14rla@gmail.com>
2024-10-29 21:06:39 +0900

    feat: firestore 백업 서비스

---
+++ lib/data/remote/back_up_service.dart
@@ -0,0 +1,37 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class BackUpService {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  Future<bool> backupToFirestore(String userId) async {
+    try {
+      final prefs = await SharedPreferences.getInstance();
+
+      final String? rootFolderJson = prefs.getString('root_folder');
+
+      if (rootFolderJson == null) {
+        debugPrint('No data found in SharedPreferences');
+        return false;
+      }
+
+      final userDocRef = _firestore.collection('backups').doc(userId);
+
+      final timestamp = DateTime.now().toIso8601String();
+
+      final backupData = {
+        'timestamp': timestamp,
+        'root_folder': rootFolderJson,
+      };
+
+      await userDocRef.collection('backups').doc(timestamp).set(backupData);
+
+      debugPrint('Backup completed successfully');
+      return true;
+    } catch (e) {
+      debugPrint('Error during backup: $e');
+      return false;
+    }
+  }
+}
