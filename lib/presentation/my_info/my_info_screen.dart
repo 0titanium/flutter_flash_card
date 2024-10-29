1804b7fe8dbf3bfa814d25c80a42f8f293dcfd73
kimhyungmin <jeen14rla@gmail.com>
2024-10-29 22:54:10 +0900

    feat: my info screen에 firestore 저장 추가

--- lib/presentation/my_info/my_info_screen.dart
+++ lib/presentation/my_info/my_info_screen.dart
@@ -1,13 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_flash_card/presentation/components/flash_card_app_bar/flash_card_app_bar.dart';
+import 'package:flutter_flash_card/presentation/my_info/my_info_model.dart';
+import 'package:flutter_flash_card/presentation/providers/flash_card_auth_provider.dart';
+import 'package:provider/provider.dart';
 
-class MyInfoScreen extends StatelessWidget {
+class MyInfoScreen extends StatefulWidget {
   const MyInfoScreen({super.key});
 
   @override
+  State<MyInfoScreen> createState() => _MyInfoScreenState();
+}
+
+class _MyInfoScreenState extends State<MyInfoScreen> {
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: FlashCardAppBar(appBarTitle: '내 정보'),
+    final flashCardAuthProvider = context.watch<FlashCardAuthProvider>();
+    final myInfoModel = context.watch<MyInfoModel>();
+
+    return SafeArea(
+      child: Scaffold(
+        appBar: const FlashCardAppBar(appBarTitle: '내 정보'),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(
+                onPressed: () {
+                  myInfoModel.uploadToCloud(flashCardAuthProvider.flashCardUser?.id);
+                },
+                child: Text('클라우드에 저장하기'),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
