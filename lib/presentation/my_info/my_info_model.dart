4548283ab28cca0b5794f3243508b75f6d32f256
kimhyungmin <jeen14rla@gmail.com>
2024-11-06 16:30:57 +0900

    feat: 모델에서 auth provider 사용

--- lib/presentation/my_info/my_info_model.dart
+++ lib/presentation/my_info/my_info_model.dart
@@ -1,15 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_flash_card/data/remote/back_up_service.dart';
+import 'package:flutter_flash_card/presentation/providers/flash_card_auth_provider.dart';
 
 class MyInfoModel extends ChangeNotifier {
   final BackUpService _backUpService;
+  final FlashCardAuthProvider _flashCardAuthProvider;
+  List<String> _backUpList = [];
 
-  MyInfoModel({required BackUpService backUpService})
-      : _backUpService = backUpService;
+  List<String> get backUpList => _backUpList;
 
-  Future<void> uploadToCloud(String? userId) async {
+  MyInfoModel({
+    required BackUpService backUpService,
+    required FlashCardAuthProvider flashCardAuthProvider,
+  })  : _backUpService = backUpService,
+        _flashCardAuthProvider = flashCardAuthProvider;
+
+  Future<List<String>> loadBackUpList() async {
+    final userId = _flashCardAuthProvider.flashCardUser?.id;
+
+    if (userId != null) {
+      _backUpList = await _backUpService.getBackupsList(userId);
+    }
+    notifyListeners();
+
+    return _backUpList;
+  }
+
+  Future<void> uploadToCloud() async {
+    final userId = _flashCardAuthProvider.flashCardUser?.id;
+
     if (userId != null) {
       await _backUpService.backupToFirestore(userId);
+      await loadBackUpList();
     }
   }
 }
