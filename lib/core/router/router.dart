83c3164fbfd05e9662796aa0380ddcd84b26dafa
kimhyungmin <jeen14rla@gmail.com>
2024-11-06 16:30:36 +0900

    feat: auth provider 생성자 주입

--- lib/core/router/router.dart
+++ lib/core/router/router.dart
@@ -1,9 +1,13 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_flash_card/data/local/data_service.dart';
 import 'package:flutter_flash_card/data/remote/back_up_service.dart';
+import 'package:flutter_flash_card/data/repository_impl/auth_repository_impl.dart';
 import 'package:flutter_flash_card/domain/model/deck.dart';
 import 'package:flutter_flash_card/domain/model/folder.dart';
 import 'package:flutter_flash_card/domain/model/learning_card.dart';
+import 'package:flutter_flash_card/domain/use_case/google_sign_in_use_case.dart';
+import 'package:flutter_flash_card/domain/use_case/google_sign_out_use_case.dart';
 
 import 'package:flutter_flash_card/presentation/components/flash_card_bottom/flash_card_bottom.dart';
 import 'package:flutter_flash_card/presentation/components/flash_card_bottom/flash_card_bottom_model.dart';
@@ -17,16 +21,26 @@ import 'package:flutter_flash_card/presentation/home/home_screen.dart';
 import 'package:flutter_flash_card/presentation/home/home_screen_model.dart';
 import 'package:flutter_flash_card/presentation/my_info/my_info_model.dart';
 import 'package:flutter_flash_card/presentation/my_info/my_info_screen.dart';
+import 'package:flutter_flash_card/presentation/providers/flash_card_auth_provider.dart';
 import 'package:flutter_flash_card/presentation/review_card/review_card_model.dart';
 import 'package:flutter_flash_card/presentation/review_card/review_card_screen.dart';
 import 'package:flutter_flash_card/presentation/sign_in/sign_in_screen.dart';
 import 'package:flutter_flash_card/presentation/view_card/view_card_model.dart';
 import 'package:flutter_flash_card/presentation/view_card/view_card_screen.dart';
 import 'package:go_router/go_router.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:provider/provider.dart';
 
 final GlobalKey<NavigatorState> rootNavigatorKey = GlobalKey<NavigatorState>();
 final GlobalKey<NavigatorState> shellNavigatorKey = GlobalKey<NavigatorState>();
+
+final googleSignIn = GoogleSignIn();
+final firebaseAuth = FirebaseAuth.instance;
+
+final authRepository = AuthRepositoryImpl(googleSignIn, firebaseAuth);
+
+final googleSignInUseCase = GoogleSignInUseCase(authRepository);
+final googleSignOutUseCase = GoogleSignOutUseCase(authRepository);
 
 final GoRouter router = GoRouter(
   navigatorKey: rootNavigatorKey,
@@ -126,7 +140,11 @@ final GoRouter router = GoRouter(
           path: '/my_info',
           builder: (context, state) {
             return ChangeNotifierProvider(
-              create: (_) => MyInfoModel(backUpService: BackUpService()),
+              create: (_) => MyInfoModel(
+                  backUpService: BackUpService(),
+                  flashCardAuthProvider: FlashCardAuthProvider(
+                      googleSignInUseCase: googleSignInUseCase,
+                      googleSignOutUseCase: googleSignOutUseCase)),
               child: const MyInfoScreen(),
             );
           },
