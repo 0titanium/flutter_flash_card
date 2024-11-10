315a0762ba0dce060a472ebfd4b3f3299a064907
kimhyungmin <jeen14rla@gmail.com>
2024-11-10 17:48:25 +0900

    feat: delete account 추가

--- lib/domain/repository/auth_repository.dart
+++ lib/domain/repository/auth_repository.dart
@@ -3,4 +3,5 @@ import 'package:flutter_flash_card/domain/model/flash_card_user.dart';
 abstract interface class AuthRepository {
   Future<FlashCardUser> signInWithGoogle();
   Future<void> signOutWithGoogle();
+  Future<void> deleteAccount();
 }
