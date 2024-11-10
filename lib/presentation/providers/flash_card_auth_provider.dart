1898de7459b9e7b014d279ab6d139366ffb48e9d
kimhyungmin <jeen14rla@gmail.com>
2024-11-10 17:49:18 +0900

    feat: delete account use case 추가

--- lib/presentation/providers/flash_card_auth_provider.dart
+++ lib/presentation/providers/flash_card_auth_provider.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_flash_card/domain/model/flash_card_user.dart';
+import 'package:flutter_flash_card/domain/use_case/firebase_auth_delete_acoount_use_case.dart';
 import 'package:flutter_flash_card/domain/use_case/google_sign_in_use_case.dart';
 import 'package:flutter_flash_card/domain/use_case/google_sign_out_use_case.dart';
 
 class FlashCardAuthProvider extends ChangeNotifier {
   final GoogleSignInUseCase _googleSignInUseCase;
   final GoogleSignOutUseCase _googleSignOutUseCase;
+  final FirebaseAuthDeleteAccountUseCase _firebaseAuthDeleteAccountUseCase;
 
   FlashCardUser? _flashCardUser;
   bool _isLoading = false;
@@ -20,8 +22,10 @@ class FlashCardAuthProvider extends ChangeNotifier {
   FlashCardAuthProvider({
     required GoogleSignInUseCase googleSignInUseCase,
     required GoogleSignOutUseCase googleSignOutUseCase,
+    required FirebaseAuthDeleteAccountUseCase firebaseAuthDeleteAccountUseCase,
   })  : _googleSignInUseCase = googleSignInUseCase,
-        _googleSignOutUseCase = googleSignOutUseCase;
+        _googleSignOutUseCase = googleSignOutUseCase,
+        _firebaseAuthDeleteAccountUseCase = firebaseAuthDeleteAccountUseCase;
 
   Future<void> signInWithGoogle() async {
     try {
@@ -46,6 +50,21 @@ class FlashCardAuthProvider extends ChangeNotifier {
 
       await _googleSignOutUseCase.execute();
       _flashCardUser = null;
+    } catch (e) {
+      _error = e.toString();
+    } finally {
+      _isLoading = false;
+      notifyListeners();
+    }
+  }
+
+  Future<void> deleteAccount() async {
+    try {
+      _isLoading = true;
+      _error = null;
+      notifyListeners();
+
+      await _firebaseAuthDeleteAccountUseCase.execute();
     } catch (e) {
       _error = e.toString();
     } finally {
