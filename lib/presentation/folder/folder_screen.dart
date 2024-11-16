559572bc689fccf9b1603f9452f98e07fd1c2f57
kimhyungmin <jeen14rla@gmail.com>
2024-11-16 15:28:58 +0900

    feat: 덱 추가 UI 다이얼로그로 변경

--- lib/presentation/folder/folder_screen.dart
+++ lib/presentation/folder/folder_screen.dart
@@ -20,53 +20,112 @@ class _FolderScreenState extends State<FolderScreen> {
       appBar: FlashCardAppBar(appBarTitle: folderModel.folderData.name),
       body: Column(
         children: [
-          Padding(
-            padding: const EdgeInsets.fromLTRB(8.0, 16.0, 8.0, 8.0),
-            child: Container(
-              decoration: BoxDecoration(
-                border: Border.all(),
-                borderRadius: BorderRadius.circular(8),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    const Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Text(
-                        '덱 추가',
-                        style: TextStyle(fontSize: 16.0),
-                      ),
-                    ),
-                    Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Container(
-                          decoration: BoxDecoration(
-                            border: Border.all(),
-                            borderRadius: BorderRadius.circular(8.0),
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.fromLTRB(8.0, 0, 8.0, 0),
-                            child: TextField(
-                              controller: folderModel.deckNameController,
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Divider(height: 1.0),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              InkWell(
+                onTap: () {
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        title: const Text('폴더 추가'),
+                        content: TextField(
+                          controller: folderModel.deckNameController,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8.0),
                             ),
+                            hintText: '덱 명을 입력하세요',
                           ),
                         ),
-                      ),
-                    ),
-                    IconButton(
-                      onPressed: () {
-                        folderModel.createDeck();
-                      },
-                      icon: const Icon(Icons.archive),
-                    ),
-                  ],
+                        actions: [
+                          TextButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: const Text('취소'),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              folderModel.createDeck();
+                              Navigator.of(context).pop();
+                            },
+                            child: const Text('추가'),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                },
+                child: const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+                  child: Row(
+                    children: [
+                      Icon(Icons.archive),
+                      SizedBox(width: 4.0),
+                      Text('덱 추가'),
+                    ],
+                  ),
                 ),
               ),
-            ),
+            ],
           ),
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Divider(height: 1.0),
+          ),
+          // Padding(
+          //   padding: const EdgeInsets.fromLTRB(8.0, 16.0, 8.0, 8.0),
+          //   child: Container(
+          //     decoration: BoxDecoration(
+          //       border: Border.all(),
+          //       borderRadius: BorderRadius.circular(8),
+          //     ),
+          //     child: Padding(
+          //       padding: const EdgeInsets.all(8.0),
+          //       child: Row(
+          //         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          //         children: [
+          //           const Padding(
+          //             padding: EdgeInsets.all(8.0),
+          //             child: Text(
+          //               '덱 추가',
+          //               style: TextStyle(fontSize: 16.0),
+          //             ),
+          //           ),
+          //           Expanded(
+          //             child: Padding(
+          //               padding: const EdgeInsets.all(8.0),
+          //               child: Container(
+          //                 decoration: BoxDecoration(
+          //                   border: Border.all(),
+          //                   borderRadius: BorderRadius.circular(8.0),
+          //                 ),
+          //                 child: Padding(
+          //                   padding: const EdgeInsets.fromLTRB(8.0, 0, 8.0, 0),
+          //                   child: TextField(
+          //                     controller: folderModel.deckNameController,
+          //                   ),
+          //                 ),
+          //               ),
+          //             ),
+          //           ),
+          //           IconButton(
+          //             onPressed: () {
+          //               folderModel.createDeck();
+          //             },
+          //             icon: const Icon(Icons.archive),
+          //           ),
+          //         ],
+          //       ),
+          //     ),
+          //   ),
+          // ),
           Expanded(
             child: ListView.builder(
               padding: const EdgeInsets.all(8),
