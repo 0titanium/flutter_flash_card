531f51b4168f6ca066ac5f5eea8b4844b4ed274d
kimhyungmin <jeen14rla@gmail.com>
2024-11-21 17:17:58 +0900

    feat: 방문 폴더 기록 제거 UI 추가

--- lib/presentation/home/home_screen.dart
+++ lib/presentation/home/home_screen.dart
@@ -15,57 +15,91 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    final homeScreenModel = context.watch<HomeScreenModel>();
+    final homeModel = context.watch<HomeScreenModel>();
     final flashCardBottomModel = context.watch<FlashCardBottomModel>();
 
     return Scaffold(
       appBar: const FlashCardAppBar(appBarTitle: '플래시카드'),
-      body: homeScreenModel.savedFolders.isEmpty
-          ? const Text('최근 방문한 폴더가 없습니다')
-          : Column(
-              children: [
-                const Padding(
-                  padding: EdgeInsets.all(16.0),
-                  child: Divider(height: 1.0),
+      body: Column(
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Divider(height: 1.0),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(left: 16.0),
+                child: Text(
+                  '최근 방문한 폴더',
+                  style: TextStyle(fontSize: 16.0),
                 ),
-                const Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.only(left: 16.0),
-                      child: Text(
-                        '최근 방문한 폴더',
-                        style: TextStyle(fontSize: 16.0),
-                      ),
-                    ),
-                  ],
+              ),
+              InkWell(
+                onTap: () {
+                  homeModel.showAllHiddenButtons();
+                },
+                child: const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+                  child: Row(
+                    children: [
+                      Icon(Icons.settings),
+                      SizedBox(width: 4.0),
+                      Text('기록 관리'),
+                    ],
+                  ),
                 ),
-                const Padding(
-                  padding: EdgeInsets.all(16.0),
-                  child: Divider(height: 1.0),
-                ),
-                Expanded(
+              ),
+            ],
+          ),
+          const Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Divider(height: 1.0),
+          ),
+          homeModel.savedFolders.isEmpty
+              ? const Text('최근 방문한 폴더가 없습니다')
+              : Expanded(
                   child: ListView.builder(
                     padding: const EdgeInsets.all(8),
-                    itemCount: homeScreenModel.savedFolders.length,
+                    itemCount: homeModel.savedFolders.length,
                     itemBuilder: (context, index) {
                       return Card(
                         child: ListTile(
                           onTap: () {
                             flashCardBottomModel.setIndex(1);
                             context.go(
-                              '/folder_list/:${homeScreenModel.savedFolders[index].name}',
-                              extra: homeScreenModel.savedFolders[index],
+                              '/folder_list/:${homeModel.savedFolders[index].name}',
+                              extra: homeModel.savedFolders[index],
                             );
                           },
-                          title: Text(homeScreenModel.savedFolders[index].name),
+                          title: Text(homeModel.savedFolders[index].name),
+                          trailing: homeModel.isEditing[index]
+                              ? Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Row(
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: [
+                                        IconButton(
+                                          onPressed: () {
+                                            homeModel.deleteVisitedFolder(
+                                                homeModel.savedFolders[index]);
+                                          },
+                                          icon: const Icon(Icons.delete),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                )
+                              : null,
                         ),
                       );
                     },
                   ),
                 ),
-              ],
-            ),
+        ],
+      ),
     );
   }
 }
